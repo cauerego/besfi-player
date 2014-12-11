b1320df8d843d6a2a0e27251d5b27ecb3d503b27
Caue Rego <caue@cregox.com>
2014-12-11 12:04:18 -0200

    add playlist refresh interval
    also a little debug info there and changing a var name

--- video player/Assets/scripts/Playlist.js
+++ video player/Assets/scripts/Playlist.js
@@ -1,25 +1,33 @@
 ﻿#pragma strict
 
+var refreshInterval : int; // in seconds
+private var lastRefreshed : float;
 static var url = new Array();
 
 function Start ()
 {
+	lastRefreshed = Time.time;
+	
 	var urlBase = "http://www.youtube.com/watch?v=";
 	
 	uQuery.Get("https://www.dropbox.com/s/3hzmj699h5p3b23/video%20list.txt?dl=1", function(data, xhr : uQueryXHR)
 	{
-		var jsonArray : json = json.fromString(data);
+		var jsonData : json = json.fromString(data);
 		
-		for (var _this in jsonArray.values)
+		for (var _this in jsonData.values)
 		{
 			url.Add(_this.toString());
 		}
 		
+		Debug.Log( "[mvPlayer] "+ lastRefreshed +" refreshing playlist with: " + jsonData.stringify() );
 		BroadcastMessage("Refresh", SendMessageOptions.DontRequireReceiver);
 	});
 }
 
 function Update ()
 {
-	
+	if (refreshInterval > 0 && Time.time > lastRefreshed + refreshInterval)
+	{
+		Start();
+	}
 }
